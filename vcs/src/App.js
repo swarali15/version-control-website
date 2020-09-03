288f5aea1d9ce8c9af52b9350b55998a22a3ba8b
swarali15 <swarali.purandare18@vit.edu>
2020-09-03 15:46:15 +0530

    done with login, register , repo ..incomplete route

---
+++ vcs/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar.js'
+import Register from './components/Register'
+import Login from './components/Login'
+import Repository from './components/RepositoryPage'
+import { BrowserRouter as Router } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+    <div >
+       <Navbar/>
+      <header >
+       
+        <Login/>
+      </header>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
